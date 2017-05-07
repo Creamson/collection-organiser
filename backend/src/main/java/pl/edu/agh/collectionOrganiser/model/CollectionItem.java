2c9a9fd5213d3eb2330dba1ae3a3a31495ed3b9b
Jacek <jacek.stob@gmail.com>
2017-05-08 00:53:49 +0200

    working collection creation

---
+++ backend/src/main/java/pl/edu/agh/collectionOrganiser/model/CollectionItem.java
@@ -0,0 +1,4 @@
+package pl.edu.agh.collectionOrganiser.model;
+
+public class CollectionItem {
+}
---
+++ backend/src/main/java/pl/edu/agh/collectionOrganiser/mongoRepos/CollectionRepository.java
@@ -0,0 +1,4 @@
+package pl.edu.agh.collectionOrganiser.mongoRepos;
+
+public interface CollectionRepository {
+}
